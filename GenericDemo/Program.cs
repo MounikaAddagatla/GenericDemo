9f920dd53b8c0778072b12f413ba4118c6a020df
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-01 12:56:45 +0530

    [Mounika] Added: Maximum Of Three Integers

---
+++ GenericDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics Demo Examples");
+            Generics<int> genericDemo = new Generics<int>();
+            genericDemo.MaximumOfNumber(20, 50, 25);
+            Console.ReadLine();
+        }
+    }
+    
+}
